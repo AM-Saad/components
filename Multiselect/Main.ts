4e853ad33ea9fe8cd70ff468da3c32db228c4676
Abdelrahman Saad <74246087+AM-Saad@users.noreply.github.com>
2023-02-26 21:49:36 +0200

    Create Main.ts
    MultiSelect Component
    Props:
    - options: an array of objects to be displayed in the dropdown list
    - multiple: a boolean indicating whether multiple options can be selected
    - closeOnSelect: a boolean indicating whether the dropdown list should close after selecting an option
    - placeholder: a string to be displayed as a placeholder in the input field
    - id: a string representing the ID of the component
    - label: a string representing the name of the field
    - trackBy: a string representing the unique key to track items by in the options array
    - disabled: a boolean indicating whether the component is disabled
    - input: a function to be called when a value is selected, with the selected items passed as an argument
    - preSelected: an optional array of pre-selected items
    - refresh: a prop to force the component to refresh when it changes
    
    Functions:
    - compareStr: a function to compare a string against a search pattern
    - search: a function to filter options based on user input
    - selectItem: a function to handle the selection of an item
    - remove: a function to remove an item from the list of selected items
    - checkIfSelected: a function to check whether an item is selected
    - closeList: a function to close the dropdown list
    */

---
+++ Multiselect/Main.ts
@@ -0,0 +1,134 @@
+
+import React, { useState, useEffect, useRef } from 'react'
+import BadgeWithDelete from './BadgeWithDelete';
+
+function compareStr(name: string, str: string) {
+    const pattern = str.split("").map((x) => `(?=.*${x})`).join("");
+    return name.match(new RegExp(`${pattern}`, "i"));
+}
+
+
+interface Props {
+    options: any[];
+    multiple: boolean;
+    closeOnSelect?: boolean;
+    placeholder: string;
+    id: string,
+    label: string
+    trackBy: string
+    disabled: boolean
+    input: (items: any) => void,
+    preSelected?: any[]
+    refresh?: any
+}
+
+
+const MultiSelect: React.FC<Props> = ({ options, multiple, closeOnSelect, placeholder, id, label, trackBy, disabled, input, preSelected = [], refresh }) => {
+    const [selected, setSelected] = useState<any[]>(preSelected)
+    const [filtered, setFiltered] = useState<any[]>(options)
+    const [listIsOpen, setListIsOpen] = useState<boolean>(false)
+    const [isFirstRender, setIsFirstRender] = useState<boolean>(true)
+
+    const itemsMenu: any = useRef<HTMLDivElement>(null)
+    const searchInput = useRef<HTMLInputElement>(null)
+
+    const search = (e: any) => {
+        const val = e.target.value
+        if (val === '' || !val) return setFiltered(options)
+        const matchedItems = options.filter((opt) => compareStr(opt[label], val));
+        setFiltered(matchedItems)
+    }
+    const selectItem = (val: string) => {
+
+        const exist = selected.find((i: any) => i[trackBy] === val)
+        if (exist) {
+            const items = selected.filter((opt: any) => opt[trackBy] !== val)
+            return setSelected(items)
+        }
+
+        const item = options.find(i => i[trackBy] === val)
+        if (item) {
+            if (multiple) {
+                setSelected((prev: any) => { return [...prev, item] });
+            } else {
+                setSelected([item])
+            }
+
+            if (closeOnSelect) setListIsOpen(false)
+        }
+
+    }
+    const remove = (val: string) => {
+        const items = selected.filter((opt: any) => opt[trackBy] !== val)
+        setSelected(items)
+        setListIsOpen(true)
+    }
+    const checkIfSelected = (val: string) => selected.findIndex((opt: any) => opt[trackBy] === val) !== -1
+
+    const closeList = (e: any) => {
+
+        if (itemsMenu.current && !itemsMenu.current.contains(e.target)) {
+            setListIsOpen(false)
+        }
+    }
+    useEffect(() => {
+        if (!isFirstRender) {
+            input(selected)
+        }
+        setIsFirstRender(false)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selected])
+
+    useEffect(() => {
+        setSelected([])
+        document.addEventListener('mousedown', closeList)
+        return () => document.removeEventListener('mousedown', closeList)
+    }, [refresh]);
+
+
+    return (
+        <>
+            <div id={id} className="multiple-select bg-white border border-gray-200 p-1 rounded-md w-full">
+                <div ref={itemsMenu} onClick={() => setListIsOpen(true)}>
+                    <div className={`selected-list flex items-center flex-wrap gap-1 ${(!multiple && selected.length === 1) && 'p-1'}`}>
+                        {(selected !== null && selected.length > 0) && selected.map((option: any, idx: number) => {
+                            return <>
+                                {multiple && <BadgeWithDelete key={idx - 1} id={option[trackBy]} onDelete={remove} label={option[label]} />}
+                                {!multiple && <p key={option[trackBy]} className='text-xs'>{option[label]}</p>}
+                            </>
+                        })}
+                    </div>
+
+                    {(listIsOpen || (!listIsOpen && multiple) || (!multiple && selected.length === 0)) &&
+                        <input
+                            className='multiple-select_input my-1 focus:outline-none w-full pl-1 text-xs'
+                            type="text"
+                            placeholder={placeholder}
+                            ref={searchInput}
+                            onChange={search}
+                        />}
+
+
+                    {listIsOpen && <ul className='multiple-select_list'>
+                        {filtered.length > 0 && filtered.map((option, idx) =>
+                            <li
+                                className={`p-2 text-xs my-1 rounded-md cursor-pointer hover:bg-gray-200  ${checkIfSelected(option[trackBy]) ? 'font-medium bg-gray-50' : ''}`}
+                                key={option[label] + idx}
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    selectItem(option[trackBy])
+                                }}
+                            >
+                                {option[label]}
+                            </li>
+                        )}
+
+                        {filtered.length === 0 && <p className='text-xs text-gray-600 my-2'>Nothing here</p>}
+                    </ul>}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MultiSelect
